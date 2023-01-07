bd8af6a1b95922942f975348b2ed5f3fa6045a98
Shakespeare <emirkaan184@gmail.com>
2023-01-08 00:15:23 +0300

    Database Connection

--- Database/mongo.js
+++ Database/mongo.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-
+const env = require('dotenv')
+env.config();
 mongoose
-    .connect('mongodb://localhost:27017/backend')
+    .connect(process.env.MONGODB)
     .then(() => console.log('Connected to DB'))
     .catch((err) => console.log(err));
