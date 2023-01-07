edd1198fd4f3bcff9db65d98eb7477d4ce23de3e
Shakespeare <emirkaan184@gmail.com>
2023-01-07 23:47:13 +0300

    Port

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use('/api/v1/auth',authRoute);
 
 app.use('/api/v1', isAuth ,groceriesRoute);
 
-app.listen(PORT, () => `Port :${PORT}`);
+app.listen(process.env.PORT || PORT, () => `Port :${PORT}`);
 
 
 
