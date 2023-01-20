0686a8ec74001fed4ce832c7d937789430d6b00a
Serhat Merak <42006836+merakserhat@users.noreply.github.com>
2023-01-20 20:54:56 +0300

    Update veryTokenHandler.js

--- controller/authController/veryTokenHandler.js
+++ controller/authController/veryTokenHandler.js
@@ -8,12 +8,7 @@ module.exports.verifyToken = async (req, res) => {
   try {
     const { bearerToken } = req.body;
 
-    const tokenDecode = jwt.verify(bearerToken, process.env.SECRET_KEY);
-
-    if (tokenDecode.exp < new Date().getTime()) {
-      // expire olmasına 15dk kalmış, yine de expire olmuş diyelim hata çıkmasın
-      return res.status(401).json({ message: "error", error: "Token expired" });
-    }
+    jwt.verify(bearerToken, process.env.SECRET_KEY);
 
     return res.status(200).json({ message: "success" });
   } catch (error) {
